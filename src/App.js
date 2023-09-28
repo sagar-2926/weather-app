f136bf5f5685a1421f5abb5bf298f8df90c695af
sagar-wandile <wandilesagar2926@gmail.com>
2023-09-28 11:10:42 +0530

    changes add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [weatherData,setWeatherData] = useState({});
+  const [city , setCity] = useState("Pune");
+  const [weatherDiscription,setWeatherDiscription] = useState("");
+
+ async function loadWeatherData() {
+    let response = ""
+    try {
+
+      response =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6c20b7414d083408cadcb2f16f510ca4`)
+      setWeatherData(response.data);
+    } 
+    catch (error)  {
+      console.log(error);
+    }
+  }
+   useEffect (() => {
+    loadWeatherData();
+   } ,[])
+
+   useEffect (() => {
+      loadWeatherData ();
+   }, [city]);
+   useEffect (() => {
+
+    setWeatherDiscription(`${weatherData?.weather?.[0]?.main }
+    (${weatherData?.weather?.[0]?.main})`)
+   }, [weatherData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='main-container'>
 
-export default App;
+     <p className='main-heading'> Today Weather <i class="fa-solid fa-cloud-showers-heavy"></i></p>
+     <p className='heading-city'>{weatherData?.name}</p>
+     <p className='main-tempature'>Temperature:{(weatherData?.main?.temp - 273).
+     toFixed(2)}°C</p>
+     <p className='main-visibility'>Visibility : {weatherData?.visibility} meters</p>
+       <p className='main-discription'> Discription :{weatherDiscription}</p> 
+       <input className='input-box' type="text"  value={city} onChange={(e) => {
+      setCity(e.target.value );
+     }} ></input> 
+    </div>
+   
+  )
+}
